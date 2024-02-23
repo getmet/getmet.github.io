4901551a6a2ce6e40aa84c427ee13c57244664d4
getmet <120913144+getmet@users.noreply.github.com>
2024-02-23 15:13:52 +0700

    Update getspotify.js

--- scripts/getspotify.js
+++ scripts/getspotify.js
@@ -1,3 +1,11 @@
+function getClientData(){
+  let clientdata={
+    id:'970d411408804c6f811075fbfd7b432f',
+    c_secret:'a7fe6210e4164fc9973def44286735de'
+  };
+  return clientdata;
+}
 function getToken(){
+  let clientdata=getClientData();
   
 }
