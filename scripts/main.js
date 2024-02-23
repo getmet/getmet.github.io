f4c39d88d8d03bb4ec33d093d8e428ee039224cf
getmet <120913144+getmet@users.noreply.github.com>
2024-02-23 13:54:07 +0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+function getHead(ptype='home'){
+  let head=document.head;
+  let vp=document.createElement('meta');
+  meta.name='viewport';
+  meta.content='width=device-width, initial-scale=1.0';
+  head.appendChild(vp);
+  let title=document.title;
+  if(ptype==='home'){
+    title='home';
+  }
+
+}
+function getHeader(){
+  let header=document.createElement('header');
+  document.body.insertAdjacentHTML('afterbegin',header)
+}
