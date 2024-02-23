7d3e320e57ca8e77e07a647c6c9a93b35aa56931
getmet <120913144+getmet@users.noreply.github.com>
2024-02-23 20:53:27 +0700

    Update getspotify.js

--- scripts/getspotify.js
+++ scripts/getspotify.js
@@ -1,66 +1,69 @@
 function setCookie(cname,cvalue,exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-  
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
     }
-    return "";
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
   }
-  
+  return "";
+}
+
 function checkCookie(cname) {
-    let cookie = getCookie(cname);
-    return cookie!==false;
+  let cookie = getCookie(cname);
+  return cookie!==false;
 }
 function getClientData(){
-  let clientdata={
-    id:'970d411408804c6f811075fbfd7b432f',
-    c_secret:'a7fe6210e4164fc9973def44286735de'
-  };
-  return clientdata;
+let clientdata={
+  id:'970d411408804c6f811075fbfd7b432f',
+  c_secret:'a7fe6210e4164fc9973def44286735de'
+};
+return clientdata;
 }
 console.log(checkCookie('ty'))
 async function getToken(){
-  let clientdata=getClientData();
+let clientdata=getClientData();
 
-  const token = await fetch("https://accounts.spotify.com/api/token", {
-    method: 'POST',
-    headers: {
-        'Authorization': 'Basic ' + btoa(clientdata.id + ':' + clientdata.c_secret),
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: 'grant_type=client_credentials'
-  }).then((data) => data.json()).then((accessObject) => accessObject);
-  setCookie('tn',token.access_token,3.6);
-  return token.access_token;
+const token = await fetch("https://accounts.spotify.com/api/token", {
+  method: 'POST',
+  headers: {
+      'Authorization': 'Basic ' + btoa(clientdata.id + ':' + clientdata.c_secret),
+      'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: 'grant_type=client_credentials'
+}).then((data) => data.json()).then((accessObject) => accessObject);
+setCookie('tn',token.access_token,3.6);
+return token.access_token;
 }
 async function getSearchResult(){
-  let searchtype=document.getElementsByClassName('searchtype');
-  let token=await getToken();
+let searchtype=document.getElementsByClassName('searchtype');
   let address;
-  let query=document.getElementById('search-query').value;
-  for(let i=0;i<searchtype.length;i++){
-    if (searchtype[i].id==='search-isrc'){
-      address=`https://api.spotify.com/v1/search?type=track&q=isrc:${query}`;
-    }
+let query=document.getElementById('search-query').value;
+  setTimeout(async function() {
+       let token=await getToken();
+for(let i=0;i<searchtype.length;i++){
+  if (searchtype[i].id==='search-isrc'){
+    address=`https://api.spotify.com/v1/search?type=track&q=isrc:${query}`;
   }
-  let searchresult=await fetch(address, {
-    headers: {
-      Authorization: `Bearer ${token}`,
+}
+let searchresult=await fetch(address, {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  }
+})
+console.log(await searchresult.json());
     }
-  })
-  console.log(await searchresult.json());
+    ,3000);
 }
