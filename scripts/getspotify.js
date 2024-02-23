04666aa6e497ee41d2ddd1d28964259c20a45a7c
getmet <120913144+getmet@users.noreply.github.com>
2024-02-23 16:54:42 +0700

    Add files via upload

--- scripts/getspotify.js
+++ scripts/getspotify.js
@@ -1,3 +1,5 @@
+import "cookie.js";
+checkCookie('token');
 function getClientData(){
   let clientdata={
     id:'970d411408804c6f811075fbfd7b432f',
@@ -5,7 +7,34 @@ function getClientData(){
   };
   return clientdata;
 }
-function getToken(){
+async function getToken(){
   let clientdata=getClientData();
-  
+
+  const token = await fetch("https://accounts.spotify.com/api/token", {
+    method: 'POST',
+    headers: {
+        'Authorization': 'Basic ' + btoa(clientdata.id + ':' + clientdata.c_secret),
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'grant_type=client_credentials'
+  }).then((data) => data.json()).then((accessObject) => accessObject);
+  setCookie('tn',token.access_token,3.6);
+  return token.access_token;
+}
+async function getSearchResult(){
+  let searchtype=document.getElementsByClassName('searchtype');
+  let token=await getToken();
+  let address;
+  let query=document.getElementById('search-query').value;
+  for(let i=0;i<searchtype.length;i++){
+    if (searchtype[i].id==='search-isrc'){
+      address=`https://api.spotify.com/v1/search?type=track&q=isrc:${query}`;
+    }
+  }
+  let searchresult=await fetch(address, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
+  console.log(await searchresult.json());
 }
