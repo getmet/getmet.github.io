1117ab25c953d4b912f3472ce41e24a867a5e4a0
getmet <120913144+getmet@users.noreply.github.com>
2024-02-23 14:01:18 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 function getHead(ptype='home'){
   let head=document.head;
   let vp=document.createElement('meta');
-  meta.name='viewport';
-  meta.content='width=device-width, initial-scale=1.0';
+  vp.name='viewport';
+  vp.content='width=device-width, initial-scale=1.0';
   head.appendChild(vp);
   let title=document.title;
   if(ptype==='home'){
