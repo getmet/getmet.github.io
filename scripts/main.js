3d85a94e753603b7e6fb5542f52233b18196ebf3
getmet <120913144+getmet@users.noreply.github.com>
2024-02-23 14:13:53 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,27 @@
+function loadstyle(ptype='home',location=null){
+  if(ptype){
+    location=`/styles/${ptype}.css`;
+  }
+  let style=document.createElement('link');
+  style.rel='stylesheet';
+  style.href=location;
+  document.head.appendChild(style);
+}
 function getHead(ptype='home'){
   let vp=document.createElement('meta');
   vp.name='viewport';
   vp.content='width=device-width, initial-scale=1.0';
   document.head.appendChild(vp);
+  loadstyle(false,'/styles/main.css');
+  loadstyle();
   let title=document.title;
   if(ptype==='home'){
     document.title='home';
   }
-
+  else{
+    document.title=document.getElementById('h1');
+  }
+  
   
 }
 function getHeader(){
